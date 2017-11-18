ef428f97b205daba8a1b29059e5efb088225a8cf
David Buchan-Swanson <david.buchanswanson@gmail.com>
2017-11-18 11:25:14 +1000

    a

--- server/src/index.js
+++ server/src/index.js
@@ -1,6 +1,10 @@
 import Koa from 'koa';
+import serve from 'koa-static';
 
 const app = new Koa();
+
+const staticPath = path.join('client', 'dist');
+app.use(serve(''))
 
 app.use(ctx => { ctx.body = 'Hello, World!'; });
 
